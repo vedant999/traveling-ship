731b74c9730f2d34947d05afab4fbddef57cc084
vedant999 <95424992+vedant999@users.noreply.github.com>
2022-01-20 19:43:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,8 +33,8 @@ function draw() {
 //Uncomment the correct line to make the background move to create a forward moving effect for the boat.
   //sea.velocityY = -3;
   //sea.x = -3;
-  //sea.velocityX = -3;
-  sea.velocityX = 3;
+  sea.velocityX = -3;
+  //sea.velocityX = 3;
 
 
   
